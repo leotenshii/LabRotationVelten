82bc8d854a02feec9b7a0cccaec933c3271d8f39
leotenshii <leotenshii@gmail.com>
2024-05-16 16:05:25 +0200

    .

--- code/Comparison.R
+++ code/Comparison.R
@@ -1,16 +1,30 @@
+#---------------------------Seed------------------------------------------------
+set.seed(42)
+#---------------------------Libraries-------------------------------------------
 library(MOFA2)
-library(SingleCellMultiModal)
 library(scater)
 library(scran)
 library(StabMap)
+library(tidyverse)
+library(cluster)
 
+#library(SingleCellMultiModal)
+
+#---------------------------Dataset---------------------------------------------
 # Peripheral Blood Mononuclear Cells provided by 10x Genomics website
 # 10x Genomics Multiome technology enables simultaneous profiling of the transcriptome 
 # (using 3’ gene expression) and epigenome (using ATAC-seq) from single cells to deepen 
 # our understanding of how genes are expressed and regulated across different cell types.
-mae <- scMultiome("pbmc_10x", mode = "*", dry.run = FALSE, format = "MTX")
+
+#mae <- scMultiome("pbmc_10x", mode = "*", dry.run = FALSE, format = "MTX")
+
+# Loaded from RDS cause I cant install the SingleCellMultiModal package
+mae <- readRDS("R/Data/data.RDS")
+metadata <- mae@colData
+metadata_ct <- metadata$celltype
 
 
+#---------------------------Data preperation------------------------------------
 # Normalization RNA
 sce.rna <- experiments(mae)[["rna"]]
 
@@ -40,51 +54,55 @@ sce.atac <- sce.atac[hvgs,]
 
 logcounts_all = rbind(logcounts(sce.rna), logcounts(sce.atac))
 
-#----------------------------MOFA-----------------------------------------------
+#---------------------------MOFA------------------------------------------------
 
+# Put NAs in data 
+logcounts_allNA <- logcounts_all
 
-log_counts100 <-logcounts_all[, 1:100]
+#logcounts_allNA[953:1740, 1:ncol(logcounts_all)/2] <- NA
 
-log_counts100NA <- log_counts100
+logcounts_allNA <- readRDS("R/Data/logcountsNA.RDS")
 
-log_counts100NA[953:1740, 1:50] <- NA
-
+# Create list for MOFa
 mofa_list <- list(
-  RNA = as.matrix( log_counts100NA[1:952,]),
-  ATAC = as.matrix(log_counts100NA[953: 1740,])
+  RNA =  logcounts_allNA[1:952,],
+  ATAC = logcounts_allNA[953: 1740,]
 )
 
-
+# MOFA model and training
 model <- create_mofa(mofa_list)
+samples_metadata(model) <- as.data.frame(metadata) %>% rownames_to_column("sample")
 plot_data_overview(model)
-
 MOFAobject <- prepare_mofa(model)
 
+run_mofa(MOFAobject, outfile = "~/R/Data/model.hdf5", use_basilisk = TRUE)
 
-run_mofa(MOFAobject, outfile = "Z:/Leoni/model.hdf5", use_basilisk = TRUE)
+trained_model <- load_model("~/R/Data/model.hdf5")
 
-trained_model <- load_model("Z:/Leoni/model.hdf5")
-
-
+# UMAP
 trained_model <- run_umap(trained_model)
-
-#color <- c(rep("RNA", 50), rep("Multiome", 50))
-
-plot_dimred(trained_model, method = "UMAP")
+plot_dimred(trained_model, method = "UMAP", color_by = "celltype")
 
 
-#----------------------------StabMap--------------------------------------------
-dataType = setNames(sample(c("RNA", "Multiome"), ncol(log_counts100),
-                           prob = c(0.5,0.5), replace = TRUE),
-                    colnames(log_counts100))
+#---------------------------StabMap---------------------------------------------
 
+# Seperation ATAC Multiome
+atac <- rep("ATAC", 5016)
+multiomes <- rep("Multiome", 5016)
 
+names <- c(atac, multiomes)
 
+# Assay Types
+assayType = ifelse(rownames(logcounts_all) %in% rownames(sce.rna),
+                   "rna", "atac")
+
+# List for StabMap
 assay_list = list(
-  RNA = log_counts100[assayType %in% c("rna"), random_names %in% c("RNA")],
-  Multiome = log_counts100[assayType %in% c("rna", "atac"), random_names %in% c("Multiome")]
+  ATAC = logcounts_all[assayType %in% c("atac"), names %in% c("ATAC")],
+  Multiome = logcounts_all[assayType %in% c("rna", "atac"), names %in% c("Multiome")]
 )
 
+# StabMap
 mosaicDataUpSet(assay_list)
 plot(mosaicDataTopology(assay_list))
 
@@ -92,33 +110,56 @@ stab = stabMap(assay_list,
                reference_list = c("Multiome"),
                plot = FALSE)
 
+# UMAP
 stab_umap = calculateUMAP(t(stab))
-plot(stab_umap, pch = 16, cex = 0.3, col = factor(dataType[rownames(stab)]))
 
+CellType <- setNames(metadata_ct, colnames(logcounts_all))
 
+full_umap <- merge( as.data.frame(CellType), stab_umap, by =0 )
+
+ggplot(full_umap) +
+  geom_point(aes(x = V1, y = V2, color = CellType)) +
+  theme_light()
 
 #---------------------------Comparison------------------------------------------
-plot_dimred(trained_model, method = "UMAP")
-plot(stab_umap, pch = 16, cex = 0.3, col = factor(dataType[rownames(stab)]))
+plot_dimred(trained_model, method = "UMAP", color_by = "celltype", dot_size =1)
+ggplot(full_umap) +
+  geom_point(aes(x = V1, y = V2, color = CellType), size = .1) +
+  theme_light()
+
+#---------------------------Silhoutte-------------------------------------------
+
+#StabMap
+
+full_umap <- full_join(full_umap %>% group_by(CellType) %>% summarise(n = n()) %>% mutate(k = 1:14) %>% select(-n), full_umap)
+full_umap_coords <- full_umap %>% select("V1", "V2")
+
+sil_Stab <- silhouette(full_umap$k, dist(full_umap_coords))
+sil_Stab_sum <- sil_Stab %>% 
+  as.data.frame() %>% group_by(cluster) %>% summarise(score = mean(sil_width), 
+                                                      frac_pos = sum(sil_width > 0)/n(),
+                                                      pos_score = sum((sil_width>0)*sil_width)/sum(sil_width > 0))
+
+
+#MOFA
+MOFA_cluster <- full_join(as.data.frame(trained_model@dim_red), as.data.frame(metadata) %>% rownames_to_column("sample"), by = join_by(UMAP.sample == sample)) %>%
+  full_join(.,y = full_umap %>% group_by(CellType) %>% summarise(n = n()) %>% mutate(k = 1:14) %>% select(-n), by = join_by(celltype  == CellType))
+
+sil_MOFA <- silhouette(MOFA_cluster$k, dist(as.data.frame(trained_model@dim_red)))
+sil_MOFA_sum <- sil_MOFA %>% 
+  as.data.frame() %>% group_by(cluster) %>% summarise(score = mean(sil_width), 
+                                                      frac_pos = sum(sil_width > 0)/n(),
+                                                      pos_score = sum((sil_width>0)*sil_width)/sum(sil_width > 0))
+
 
 #---------------------------Imputation------------------------------------------
 imp = imputeEmbedding(
   assay_list,
   stab,
   reference = colnames(assay_list[["Multiome"]]),
-  query = colnames(assay_list[["RNA"]]))
+  query = colnames(assay_list[["ATAC"]]))
 
 trained_model <- impute(trained_model)
-# trained_model@data$ATAC[[1]][1:10, 1:5]
+# imp$Multiome[953:963, 1:5]
 # trained_model@imputed_data$ATAC[[1]][1:10, 1:5]
 
-#---------------
-rnas <- rep("RNA", 50)
-multiomes <- rep("Multiome", 50)
-
-# Combine the names
-random_names <- c(rnas, multiomes)
-
-# Shuffle the names
-
-random_names <- sample(random_names)
